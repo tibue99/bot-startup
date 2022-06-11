12892d4f756835491ca2a89b3caa610ad302e68e
Timo <tibue69@gmail.com>
2022-06-11 17:30:27 +0200

    Fix success check with subprocess

--- startup.py
+++ startup.py
@@ -1,5 +1,6 @@
 import os
 import time
+import subprocess
 
 print("\nDeleting old sessions...")
 os.system("tmux kill-server")
@@ -8,18 +9,16 @@ print("Starting new sessions...")
 print("----------------------------------")
 time.sleep(5)
 
+names = []
+
 
 def run(path):
     name = path.split("/")[-1]
 
     os.system(f"tmux new -d -s '{name}'")
     os.system(f"tmux send-keys 'cd {path}' C-m")
-    result = os.system(f"tmux send-keys 'python3 main.py' C-m")
-
-    if result == 0:
-        print(f"Successfully started {path}")
-    else:
-        print(f"Error: Could not start {path}")
+    os.system(f"tmux send-keys 'python3 main.py' C-m")
+    names.append(name)
 
 
 home = "../home/"
@@ -27,7 +26,7 @@ home = "../home/"
 
 for dir1 in os.listdir(home):
     if "main.py" not in os.listdir(home + dir1):
-        print(f"Detected {dir1} as subfolder")
+        print(f"Info: Detected {dir1} as subfolder")
         for dir2 in os.listdir(home + dir1):  # iterate through subfolder
             if "main.py" not in os.listdir(home + dir1 + "/" + dir2):
                 print(f"Warning: Detected empty subfolder: {dir1}/{dir2}")
@@ -35,3 +34,12 @@ for dir1 in os.listdir(home):
                 run(dir1 + "/" + dir2)
     else:
         run(dir1)
+
+for name in names:
+    result = subprocess.run(['tmux', 'capture-pane', '-pt', name, '-S', '3', '-E', '10'], stdout=subprocess.PIPE)
+    output = result.stdout.decode('utf-8')
+
+    if "online" in output:
+        print(f"Successfully started {name}")
+    else:
+        print(f"Error: Could not start {name}")

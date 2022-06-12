f934685b32269b77ac86008c20438e308c51d17f
Timo <tibue69@gmail.com>
2022-06-13 01:23:50 +0200

    Fix path inside new tmux sessions

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-# The path of the bot directory, starting at the root directory
+# The path to the bot directory, starting at the directory of this script
 bot_directory = "../home/"
 
 # The name of the main file to start every bot
--- startup.py
+++ startup.py
@@ -25,10 +25,10 @@ def kill_sessions():
 def run(path, subdir=None):
     session_name = path.split("/")[-1]
     if subdir is not None:
-        session_name = f"{subdir[0]}_{session_name}"
+        session_name = f"{subdir[0].lower()}_{session_name}"
 
     os.system(f"tmux new -d -s '{session_name}'")
-    os.system(f"tmux send-keys 'cd {path}' C-m")
+    os.system(f"tmux send-keys 'cd {home + path}' C-m")
     os.system(f"tmux send-keys 'python3 {main_file}' C-m")
     names.append(session_name)
 
@@ -40,7 +40,11 @@ def iterate_directory():
         if main_file not in os.listdir(home + dir1):
             print(f"Info: Detected {dir1} as subdirectory")
             for dir2 in os.listdir(home + dir1):
-                if main_file not in os.listdir(home + dir1 + "/" + dir2):
+                subdir_path = home + dir1 + "/" + dir2
+                if not os.path.isdir(subdir_path):  # subdirectory should only contain directories, not files
+                    continue
+
+                if main_file not in os.listdir(subdir_path):
                     print(f"Warning: Detected directory without main file: {dir1}/{dir2}")
                 else:
                     run(dir1 + "/" + dir2, dir1)
@@ -52,7 +56,7 @@ def check_success():
     print("Checking sessions...\n")
     for name in names:
         found_result = False
-        result = subprocess.run(['tmux', 'capture-pane', '-pt', name, '-S', '3', '-E', '10'], stdout=subprocess.PIPE)
+        result = subprocess.run(['tmux', 'capture-pane', '-pt', name, '-S', '1', '-E', '30'], stdout=subprocess.PIPE)
         output = result.stdout.decode('utf-8')
         for line in io.StringIO(output):
             line = line.split("\n")[0]
@@ -74,7 +78,7 @@ if __name__ == "__main__":
     wait(2)
 
     iterate_directory()
-    wait(3)
+    wait(5)
 
     check_success()
     wait(0)
